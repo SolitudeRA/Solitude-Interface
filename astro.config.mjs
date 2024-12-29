3a55de5ff1094fea38d847410dbfb638866dd69d
SolitudeRA <solitude@protogalaxy.me>
2024-12-29 21:03:55 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.solitudera.com',
+    i18n: {
+        locales: ['zh', 'en', 'ja'],
+        defaultLocale: 'zh',
+        routing: {
+            prefixDefaultLocale: false
+        }
+    },
+    integrations: [tailwind(), react()]
+});
