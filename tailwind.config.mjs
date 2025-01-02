bd430b5e2222ad5ab3dce83414708ef7139cc4af
SolitudeRA <solitude@protogalaxy.me>
2025-01-02 23:53:41 +0900

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
+import {nextui} from "@nextui-org/react";
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    prefix: 'tw-',
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            sans: ['Noto Sans SC Variable', ...defaultTheme.fontFamily.sans],
+            serif: ['Noto Serif SC Variable', defaultTheme.fontFamily.serif]
+        },
+        extend: {
+            colors: {
+
+            }
+        }
+    },
+    darkMode: "class",
+    plugins: [nextui()],
 };
