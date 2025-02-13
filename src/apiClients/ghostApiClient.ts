f9405c8763976d0ec677460b6dcfcd387354748f
SolitudeRA <solitude@protogalaxy.me>
2025-02-13 23:59:24 +0900

    update post view

--- src/apiClients/ghostApiClient.ts
+++ src/apiClients/ghostApiClient.ts
@@ -63,6 +63,8 @@ export class GhostAPIClient {
         }
     }
 
+    //test
+
     private handleError(error: any): never {
         if (axios.isAxiosError(error)) {
             console.error("API Error:", {
