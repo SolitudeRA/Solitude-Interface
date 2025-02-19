7e80108b56260541a05e607b840e75db7ad12bd1
SolitudeRA <solitude@protogalaxy.me>
2025-02-19 19:49:27 +0900

    Migrating to tailwindcss 4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.solitudera.com',
-    i18n: {
-        locales: ['zh', 'en', 'ja'],
-        defaultLocale: 'zh',
-        routing: {
-            prefixDefaultLocale: false,
-        },
-    },
-    prefetch: true,
-    integrations: [tailwind(), react()],
+  site: 'https://www.solitudera.com',
+
+  i18n: {
+      locales: ['zh', 'en', 'ja'],
+      defaultLocale: 'zh',
+      routing: {
+          prefixDefaultLocale: false,
+      },
+  },
+
+  prefetch: true,
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/components/index/IndexPost.tsx
+++ src/components/index/IndexPost.tsx
@@ -1,10 +1,10 @@
-import {Fragment} from "react";
-import type {ReactElement} from "react";
-import {motion} from "motion/react";
-import {css} from "@emotion/react";
-import {Card, CardHeader, CardFooter, Image, Chip} from "@heroui/react";
+import { Fragment } from 'react';
+import type { ReactElement } from 'react';
+import { motion } from 'motion/react';
+import { css } from '@emotion/react';
+import { Card, CardHeader, CardFooter, Image, Chip } from '@heroui/react';
 
-import type {IndexHighlightPost, PostTag} from "@apiClients/ghostPosts.ts";
+import type { IndexHighlightPost, PostTag } from '@apiClients/ghostPosts.ts';
 
 export default function IndexPost(props: { post: IndexHighlightPost }) {
     const post = props.post;
@@ -13,16 +13,29 @@ export default function IndexPost(props: { post: IndexHighlightPost }) {
         <motion.div
             css={cardStyle}
             className="snap-start scroll-ml-[40px] bg-transparent"
-            initial={{opacity: 0}}
-            whileInView={{opacity: 1}}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
         >
-            <Card className="w-full h-full" isFooterBlurred isPressable onPress={() => window.open(post.url, "_blank")}>
-                <CardHeader css={cardHeaderStyle} className="absolute flex-col items-start bg-neutral-200/85 dark:bg-gray-800/85">
+            <Card
+                className="w-full h-full"
+                isFooterBlurred
+                isPressable
+                onPress={() => window.open(post.url, '_blank')}
+            >
+                <CardHeader
+                    css={cardHeaderStyle}
+                    className="absolute flex-col items-start bg-neutral-200/85 dark:bg-gray-800/85"
+                >
                     <span className="flex justify-between w-full">
                         {renderPrimaryTag(post.primary_tag)}
                         {renderTags(post.tags)}
                     </span>
-                    <span css={cardHeaderTitleStyle} className='text-nowrap font-medium'>{renderTitle(post.title)}</span>
+                    <span
+                        css={cardHeaderTitleStyle}
+                        className="text-nowrap font-medium"
+                    >
+                        {renderTitle(post.title)}
+                    </span>
                 </CardHeader>
                 <Image
                     removeWrapper
@@ -32,8 +45,11 @@ export default function IndexPost(props: { post: IndexHighlightPost }) {
                     loading="eager"
                     src={post.feature_image.toString()}
                 />
-                <CardFooter css={cardFooterStyle} className="absolute text-white font-normal bg-black/30">
-                    <p>{post.published_at.split("T")[0]}</p>
+                <CardFooter
+                    css={cardFooterStyle}
+                    className="absolute text-white font-normal bg-black/30"
+                >
+                    <p>{post.published_at.split('T')[0]}</p>
                 </CardFooter>
             </Card>
         </motion.div>
@@ -52,73 +68,73 @@ function renderPrimaryTag(primary_tag?: PostTag): ReactElement {
             color="primary"
             radius="sm"
         >
-            {primary_tag?.name || "default"}
+            {primary_tag?.name || 'default'}
         </Chip>
     );
 }
 
 function renderTags(tags?: PostTag[]): ReactElement | null {
-    if (!tags) return null
+    if (!tags) return null;
 
     return (
         <Fragment>
-            {tags.map(tag =>
-                          tag.slug.includes("series-") ? (
-                              <Chip
-                                  key={tag.slug}
-                                  css={cardHeaderTagsStyle}
-                                  variant="flat"
-                                  color="secondary"
-                                  radius="sm"
-                              >
-                                  {tag.name}
-                              </Chip>
-                          ) : null
+            {tags.map((tag) =>
+                tag.slug.includes('series-') ? (
+                    <Chip
+                        key={tag.slug}
+                        css={cardHeaderTagsStyle}
+                        variant="flat"
+                        color="secondary"
+                        radius="sm"
+                    >
+                        {tag.name}
+                    </Chip>
+                ) : null,
             )}
         </Fragment>
-    )
+    );
 }
 
 const cardStyle = css`
-    display       : inline-block;
-    width         : 19.8rem;
-    min-width     : 300px;
-    aspect-ratio  : 7 / 5;
-    border-radius : 20px;
-    margin-left   : 50px;
+    display: inline-block;
+    width: 19.8rem;
+    min-width: 300px;
+    aspect-ratio: 7 / 5;
+    border-radius: 20px;
+    margin-left: 50px;
 `;
 
 const cardHeaderStyle = css`
-    z-index : 10;
+    z-index: 10;
 `;
 
 const cardHeaderTagsStyle = css`
-    height        : 1.35rem;
-    font-size     : 0.665rem;
-    padding-left  : 0.3rem;
-    margin-bottom : 0.82rem;
+    height: 1.35rem;
+    font-size: 0.665rem;
+    padding-left: 0.3rem;
+    margin-bottom: 0.82rem;
 `;
 
 const cardHeaderTitleStyle = css`
-    font-size     : 1.125rem;
-    line-height   : 1.1rem;
-    margin-bottom : 0.1rem;
+    font-size: 1.125rem;
+    line-height: 1.1rem;
+    margin-bottom: 0.1rem;
 `;
 
 const cardCoverStyle = css`
-    width           : 100%;
-    height          : 100%;
-    border-radius   : 22px;
-    image-rendering : crisp-edges;
-    padding         : 0;
-    z-index         : 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 22px;
+    image-rendering: crisp-edges;
+    padding: 0;
+    z-index: 0;
 `;
 
 const cardFooterStyle = css`
-    height       : 2.5rem;
-    padding-left : 1.2rem;
-    font-size    : 0.9rem;
-    color        : #B2B2B2;
-    z-index      : 10;
-    bottom       : 0;
+    height: 2.5rem;
+    padding-left: 1.2rem;
+    font-size: 0.9rem;
+    color: #b2b2b2;
+    z-index: 10;
+    bottom: 0;
 `;
