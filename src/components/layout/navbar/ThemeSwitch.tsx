3cb1f2a2263c43026cc572068252629b8da1b703
SolitudeRA <solitude@protogalaxy.me>
2025-04-21 18:25:02 +0900

    Refactor in progress

--- src/components/layout/navbar/ThemeSwitch.tsx
+++ src/components/layout/navbar/ThemeSwitch.tsx
@@ -1,28 +1,13 @@
-import {Switch} from "@heroui/switch";
-import {useThemeStore} from "@stores/themeStore.ts";
-
-import {GrSun, GrMoon} from "react-icons/gr";
+import { Switch } from '@api/shadcn/components/ui/switch';
 
 export default function ThemeSwitch() {
-
-    const {themeMode, setThemeMode} = useThemeStore();
+    const { themeMode, setThemeMode } = useThemeStore();
 
     const isLightMode = themeMode === 'light';
 
-    return (
-        <Switch
-            isSelected={isLightMode}
-            classNames={{
-                wrapper: "bg-default-200"
-            }}
-            color="warning"
-            size="lg"
-            startContent={<GrSun/>}
-            endContent={<GrMoon/>}
-            className={"shadow-xl rounded-2xl"}
-            onValueChange={(isSelected: boolean) => {
-                isSelected ?  setThemeMode("light") : setThemeMode("dark")
-            }}
-        />
-    );
+    return <Switch onCheckedChange={} />;
+}
+
+function themeSwitch() {
+    
 }
